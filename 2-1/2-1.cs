6ff1a084510000eea9d7832738b994823144888a
qqq991124 <qqq991124@naver.com>
2023-03-14 19:00:50 +0900

    if-else문 활용 / 삼항연산자 활용 등 추가

--- 2-1/2-1.cs
+++ 2-1/2-1.cs
@@ -9,11 +9,22 @@ namespace _2_1
             Console.Write("임의의 정수 하나를 입력하세요: ");
             int input = int.Parse(Console.ReadLine());
 
+            // 삼항연산자 사용
             string output = input > 0 ? "양수" :  // 0 이상인 경우 양수
                 (input < 0 ? "음수" : "0");       // 미만인 경우 음수
                                                  // 둘 모두 아니면 0
             Console.WriteLine("실행 결과");
             Console.WriteLine("입력한 값은 " + output + "입니다.");
+            
+            // if - else if문 사용
+            // Console.Write("입력한 값은 ");
+            //  if(input > 0) 
+            //      Console.Write("양수");
+            //  else if(input < 0)
+            //      Console.Write("음수);
+            //  else
+            //      Console.Write("0");
+            //  Console.Write("입니다.");
         }
 
     }
--- 2-2/2-2.cs
+++ 2-2/2-2.cs
@@ -4,6 +4,8 @@ namespace _2_2
 {
     class Program
     {
+        
+
         static void Main(string[] args)
         {
             Console.Write("임의의 문자열을 입력하세요: ");
@@ -13,10 +15,29 @@ namespace _2_2
 
             foreach(char chr in input)
             {
-                
-                if (chr == 'a' || chr == 'e' || chr == 'i'
-                    || chr == 'o' || chr == 'u')
-                    count++;
+                // if문 + 논리연산자 사용
+                // if (chr == 'a' || chr == 'e' || chr == 'i'
+                //    || chr == 'o' || chr == 'u')
+                //    count++;
+                // switch문
+                switch(chr)
+                {
+                    case 'a':
+                        count++;
+                        break;
+                    case 'e':
+                        count++;
+                        break;
+                    case 'i':
+                        count++;
+                        break;
+                    case 'o':
+                        count++;
+                        break;
+                    case 'u':
+                        count++;
+                        break;
+                }
             }
 
             Console.WriteLine("임의의 문자열 속 모음의 개수: " + count);
